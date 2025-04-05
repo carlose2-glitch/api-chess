84005729cb648ab789f395ac100110a56bafb7c7
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2025-04-04 22:55:17 -0400

    configuracion para el funcionamiento en render

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { SingModule } from './sing/sing.module';
 import { LoginModule } from './login/login.module';
 import { tokenModule } from './token/token.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [SingModule, LoginModule, tokenModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    SingModule,
+    LoginModule,
+    tokenModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
